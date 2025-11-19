226b45e30edc2b9e3f3397d34f3e5c2043891ad3
Devansh <dewanshvishwakarma073@gmail.com>
2025-11-19 21:13:51 +0530

    first commit

---
+++ domtask.js
@@ -0,0 +1,15 @@
+
+function display() {
+  let img = document.querySelector("#oo");
+   img.style.backgroundImage =
+        "url('download.jpg')"
+
+ }
+
+function display2() {
+  let img = document.querySelector("#oo");
+   img.style.backgroundImage =
+        "url('images.jpg')"
+
+ }
+ 
