7c5deb0737986443b4a45625bca521f2428907c8
dewansh vishwakarma <dewanshvishwakarma073@gmail.com>
2025-11-23 18:27:22 +0530

    function in js
    today i learn function in java script and solve some problem

---
+++ function in js.js
@@ -0,0 +1,21 @@
+//Q:1 Write a function to add two numbers and return the result.
+function add(a,b){
+    return a+b 
+}
+result=add(10,20)
+console.log(result)
+
+// Write a function to check if a number is even or odd.
+function oddeven(a){
+    if(a%2==0){
+        return a
+    }
+    else{
+        console.log("odd number")
+    }
+}
+result=oddeven(5)
+console.log(result)
+
+
+
